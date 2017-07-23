478f1e3ed38f768e97e52d2072913981ad666ba3
chabat <felipechabat@gmail.com>
2017-07-23 12:58:43 -0300

    Finished navbar

---
+++ php/navbar.php
@@ -0,0 +1,31 @@
+<!-- Navbar -->
+<nav class = "navbar navbar-inverse navbar-fixed-top" id= "mynavbar">
+  <div class = "container"> <!-- switch to container-fluid!? -->
+    <!-- <a class = "navbar-brand" href = "index.html">Hotel</a> -->
+    <div class = "navbar-header">
+      <button type = "button" class = "navbar-toggle" data-toggle = "collapse" data-target = "#navbar-collapse">
+        <span class = "icon-bar"></span>
+        <span class = "icon-bar"></span>
+        <span class = "icon-bar"></span>
+      </button>
+
+      <a href="index.html" class="navbar-left"><img src="./img/logo.png" width = "159px"
+         heigth = "45px" id = navbarlogo></a>
+    </div> <!-- End Navbar Header -->
+
+    <!-- Searchbar -->
+    <input type="text" name="search" placeholder="Faça aqui sua busca" id = "searchbar">
+
+    <div class = "collapse navbar-collapse" id = "navbar-collapse">
+
+      <a href = "signin.html" class = "btn btn-default navbar-btn navbar-right
+        glyphicon glyphicon-log-in "> Entrar</a>
+      <a href = "signup.html" class = "btn btn-default navbar-btn navbar-right
+        glyphicon glyphicon-user"> Cadastre-se</a>
+      <a href = "cart.html" class = "btn btn-default navbar-btn navbar-right
+        glyphicon glyphicon glyphicon-shopping-cart "> Carrinho</a>
+
+    </div> <!-- End collapse navbar  -->
+  </div> <!-- End Container -->
+
+</nav> <!-- End Navbar -->
---
+++ signup.php
@@ -0,0 +1,18 @@
+<!-- !#()&@%  -->
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta name = "viewport" content = "width = device-width, initial scale = 1.0">
+    <meta charset = "utf-8">
+  	<meta name = "description" content = "Página de cadastro da Livraria Educativa">
+  	<title>Livraria Educativa - Cadastro</title>
+  	<link href = "css/bootstrap.css" rel = "stylesheet">
+    <link href = "css/navbar.css" rel = "stylesheet">
+  </head>
+
+  <body>
+    <?php include("./php/navbar.php"); ?>
+  </body>
+
+</html>
